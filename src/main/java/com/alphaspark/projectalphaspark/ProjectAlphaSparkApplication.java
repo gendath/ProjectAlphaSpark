6f08e70de08f897e71b200c6d70c646b8b70f1e3
gendath <sellwithperry@gmail.com>
2022-05-20 08:11:45 -0700

    Initial commit

---
+++ src/main/java/com/alphaspark/projectalphaspark/ProjectAlphaSparkApplication.java
@@ -0,0 +1,13 @@
+package com.alphaspark.projectalphaspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAlphaSparkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectAlphaSparkApplication.class, args);
+    }
+
+}
