e2d217b5a4388bed3fb24e8c99cffd60b45b5406
gendath <sellwithperry@gmail.com>
2022-05-26 13:28:01 -0700

    templated user controller

--- src/main/java/com/alphaspark/projectalphaspark/Controllers/UserController.java
+++ src/main/java/com/alphaspark/projectalphaspark/Controllers/UserController.java
@@ -1,5 +1,53 @@
 package com.alphaspark.projectalphaspark.Controllers;
 
+import com.alphaspark.projectalphaspark.Entities.BaseUser;
+import com.alphaspark.projectalphaspark.Services.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    //create
+    @PostMapping("/new")
+    public String createUser(){
+        return null;
+    }
+
+    //read all
+    @GetMapping({"/", ""})
+    public List<BaseUser> listAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    //read one
+    @GetMapping("/{userID}")
+    public BaseUser displaySingleUser(@PathVariable Long userID){
+
+        return userService.getUser(userID);
+
+
+    }
+    //update
+    @PutMapping("/update")
+    public boolean updateUser(@RequestBody BaseUser user){
+
+
+        return userService.updateUser(user);
+    }
+
+    //delete
+    @DeleteMapping("/delete")
+    public boolean deleteUser(@RequestBody BaseUser user){
+        return userService.deleteUser(user.getId());
+    }
 
 }
